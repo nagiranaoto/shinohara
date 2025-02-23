6311a941912f9daa8d5738e8acf0f69607d0fb08
nagiranaoto <nagiranaoto@gmail.com>
2025-02-24 00:25:31 +0900

    10分の1でレア猫を表示できるように修正

--- src/main/java/com/example/spring_thymeleaf_app/controller/CatController.java
+++ src/main/java/com/example/spring_thymeleaf_app/controller/CatController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
-
+import java.util.Random;
 
 @Controller
 public class CatController {
@@ -15,15 +15,28 @@ public class CatController {
     @Value("${cat.api.url}")
     private String url;
 
+    @Value("${cat.rare.url}")
+    private String rareUrl;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/cat")
     public String getCatImage(Model model) {
-  
-        CatImage[] catImages = restTemplate.getForObject(url, CatImage[].class);
-        if (catImages != null && catImages.length > 0) {
-            model.addAttribute("catImageUrl", catImages[0].getUrl());
+        Random random = new Random();
+        int randomNumber = random.nextInt(10); // 0から9までのランダムな数値を生成
+
+        if (randomNumber == 0) {
+            // 10回に1回は異なる画像を設定
+            model.addAttribute("catImageUrl", rareUrl);
+        } else {
+            // それ以外の場合は猫の画像を設定
+            CatImage[] catImages = restTemplate.getForObject(url, CatImage[].class);
+            if (catImages != null && catImages.length > 0) {
+                model.addAttribute("catImageUrl", catImages[0].getUrl());
+            } else {
+                model.addAttribute("catImageUrl", rareUrl);
+            }
         }
         return "cat";
     }
